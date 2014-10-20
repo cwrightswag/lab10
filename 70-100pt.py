d5fbfed6aa33f219a8ea8e76964331cb7c0d94a2
Caden Wright <cadw2000@gmail.com>
2014-10-20 15:21:38 -0500

    Commit 1
    swag

--- 70-100pt.py
+++ 70-100pt.py
@@ -14,4 +14,17 @@
 
 # Minus 5pts if your code has no comments
 # Minus 10pts if you only commit once to github
-#swaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaag
+
+from Tkinter import *
+root = Tk()
+# Create the canvas widget
+drawpad = Canvas(root, width=800,height=600, background='white')
+drawpad.grid(row=0, column=1)
+# House outline and roof
+sweg = drawpad.create_rectangle(280,230,520,430)
+yolo = drawpad.create_line(280,230,390,110)
+ni = drawpad.create_line(520,230,390,110)
+# Square Windows and a door
+
+
+root.mainloop()
