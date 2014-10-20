b81bdf27113f22c7388358f2eba8b904f8632c3f
Caden Wright <cadw2000@gmail.com>
2014-10-20 15:57:59 -0500

    commit 3 final product i think
    swag

--- 70-100pt.py
+++ 70-100pt.py
@@ -21,16 +21,21 @@ root = Tk()
 drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=1)
 # House outline and roof
-sweg = drawpad.create_rectangle(280,230,520,430)
-yolo = drawpad.create_line(280,230,400,110)
-ni = drawpad.create_line(520,230,400,110)
+swag = drawpad.create_rectangle(280,230,520,430, fill = 'red')
+swbg = drawpad.create_line(280,230,400,110)
+swcg = drawpad.create_line(520,230,400,110)
 # Square Windows and a door
-swua = drawpad.create_rectangle(320,270,350,300)
-swug = drawpad.create_rectangle(440,270,470,300)
-swag = drawpad.create_rectangle(410,430,380,380)
-swbg = drawpad.create_rectangle(320,380,350,410)
-swcg = drawpad.create_rectangle(440,380,470,410)
-# 
+swdg = drawpad.create_rectangle(320,270,350,300, fill = 'blue')
+sweg = drawpad.create_rectangle(440,270,470,300, fill = 'blue')
+swfg = drawpad.create_rectangle(410,430,380,380, fill = 'purple')
+swgg = drawpad.create_rectangle(320,380,350,410, fill = 'blue')
+swhg = drawpad.create_rectangle(440,380,470,410, fill = 'blue')
+# a door handle and a chimney
+swig = drawpad.create_line(520,230,520,170)
+swjg = drawpad.create_line(520,170,490,170)
+swkg = drawpad.create_line(490,170,490,200)
+swlg = drawpad.create_oval(400,400,410,410, fill = 'black')
+swmg = drawpad.create_rectangle(0,430,800,460, fill = 'green')
 
 
 
